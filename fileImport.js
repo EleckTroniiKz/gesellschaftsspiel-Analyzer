0bbc81e7112c6f02f871d8b0cc26f1baef2c156a
EleckTroniiKz <ugurtancancetin@gmx.de>
2022-02-09 16:29:30 +0100

    Adding comments to new methods and deleted getFileData(path) & saveReadData(data)

--- fileImport.js
+++ fileImport.js
@@ -2,58 +2,70 @@ const fs = require('fs');
 const LocalStorage = require('node-localstorage').LocalStorage;
 const localStorage = new LocalStorage('./DataStorage/storage');
 
-/**
- * this. ... works in fs.readfile only when the callback is an arrow function.
- */
-
-//Mabye change json with localStorage
 class fileImport {
-  constructor(fileName){
-    this.fileName = fileName;
-    this.fileData = ""
-    this.userIDs = []
-  }
+	constructor(fileName){
+		this.fileName = fileName;
+		this.fileData = ""
+		this.userIDs = []
+	}
 
+	/**
+	 * @description Deletes user out of global user list
+	 * @param {userID} number  is the ID of the user which is going to be deleted 
+	 */
 	deleteUser(userID){
 		let userList = this.getUserList();
 		let filteredUsers = userList.filter((value) => {
-					if(value.uID !== userID){
-						return value;
-					}
-				})
+			if(value.uID !== userID){
+				return value;
+				}
+			})
 		localStorage.setItem('users', JSON.stringify(filteredUsers))
 	}
 
+	/**
+	 * @description Deletes a game out of the games list of a specific user
+	 * @param {userID} number  is the ID of the user in which the game will be removed out of the users list.
+	 * @param {game} string is the name of the game which will be deleted.  
+	 */
 	deleteGame(userID, game){
 		const userList = this.getUserList();
 		for(let i = 0; i < userList.length; i++){
-			if(userList[i].uID === userID){
-				if(userList[i].games.includes(game)){
-					let filteredGames = userList[i].games.filter((value) => {if(value !== game) return value})
-					userList[i].games = filteredGames;
-					console.log(filteredGames)
-					localStorage.setItem('users', JSON.stringify(userList))
-				}
+			if(userList[i].uID === userID && userList[i].games.includes(game)){
+				let filteredGames = userList[i].games.filter((value) => {if(value !== game) return value})
+				userList[i].games = filteredGames;
+				localStorage.setItem('users', JSON.stringify(userList))
 			}
 		}
 	}
 
+	/**
+	 * @description adds game to the games list of an user
+	 * @param {userID} number is the ID of the user in which a new game is going to be added
+	 * @param {game} string is the name of the game which will be added
+	 */
 	addGame(userID, game){
 		const userList = this.getUserList();
 		for(let i = 0; i < userList.length; i++){
-			if(userList[i].uID === userID){
-				if(userList[i].games.includes(game) === false){
-					userList[i].games.push(game)
-					localStorage.setItem('users', JSON.stringify(userList))
-				}
+			if(userList[i].uID === userID && !(userList[i].games.includes(game))){				
+				userList[i].games.push(game)
+				localStorage.setItem('users', JSON.stringify(userList))
 			}
 		}
 	}
 
+	/**
+	 * @returns a JSON Object of the user storage 
+	 */
 	getUserList(){
 		return JSON.parse(localStorage.getItem('users'));
 	}
 
+	/**
+	 * @description adds user to the global user list
+	 * @param {userName} string is the name of the new user
+	 * @param {gameArr} array is an array with the games the new user has 
+	 */
 	addUser(userName, gameArr){
 		let userList = this.getUserList();
 		let userObject = {uID: this.generateUserID(), name: userName, games: gameArr}
@@ -61,48 +73,23 @@ class fileImport {
 		localStorage.setItem('users', JSON.stringify(userList))
 	}
 
-  /**  
-   * @param data: extracted data from json file
-   *  saves the data from the parameter in the class variable data. 
-   *  had to be extracted into another method because of Promise
-  */
-  saveReadData(data){
-    this.fileData = JSON.parse(data)
-    console.log(this.fileData)
-  }
+	/**
+	 * @description generates a userid and returns it
+	 * @returns a new random userID (0-99)
+	 */
+	generateUserID() {
+	let id = Math.trunc(Math.floor(Math.random() * 99));
+	while(this.userIDs.includes(id)){
+		id = Math.random() * 9999;
+	} 
+	this.userIDs.push(id)
+	return id;
+	}
 
-  /**  
-   * @param filepath: path of file in this project 
-   *  extracts the data from the storage.json and saves it
-  */ 
-  getFileData(filepath){
-    fs.readFile((filepath), (err, data) => {
-      if(err){ 
-        console.log("Couldn't Read File")
-        this.saveReadData("No Data found")
-      }
-      else{
-        let parsedData = data
-        this.saveReadData(parsedData);
-      }
-    })
-    
-  }
-
-  /**
-   * 
-   * generates UserIDs
-   * @returns userID
-   */
-  generateUserID() {
-    let id = Math.trunc(Math.floor(Math.random() * 9999));
-    while(this.userIDs.includes(id)){
-      id = Math.random() * 9999;
-    } 
-    this.userIDs.push(id)
-    return id;
-  }
-
+	/**
+	 * @description receives a nested array with user data which is then saved to the global user list
+	 * @param users a nested list of the users and the game of the users 
+	 */
 	saveUserData(users) {
 		let userList = [];
 		let gameArr = [];
@@ -124,11 +111,11 @@ class fileImport {
 		localStorage.setItem('users', JSON.stringify(userList));
 	}
 	
-  /**  
-   *  extracts data from .csv file and saves it in storage.json
-  */
-  importCSV() {
-    let filePath = `DataImport//${this.fileName}`;
+	/**  
+	 *  @description takes the file path of the import file and extracts the data from a .csv file
+	 */
+	importCSV() {
+	let filePath = `DataImport//${this.fileName}`;
 		fs.readFile(filePath, 'utf-8', (err,data) => {
 			if(err) console.log('File not found!')
 			let dataArray = data.replace(/(\r\n|\n|\r)/gm, ';;').split(";");
@@ -152,32 +139,32 @@ class fileImport {
 			}
 			this.saveUserData(importedData);
 		})
-  }
+	}
 
-  /**  TODO
-   *  extracts data from .xlsx file and saves it in storage.json
-  */
-  importEXCEL() {
-    console.log("importing EXCEL")
-  }
+	/**
+	 *  @description takes the file path of the import file and extracts the data from a .xlsx file
+	 */
+	importEXCEL() {
+		console.log("importing EXCEL")
+	}
 
-  /**  
-   *  checks filetype and then determines, which filetype it has to call the according method
-  */
-  checkFilename(){
-    const fileType = this.fileName.slice(-4)
-    if(fileType[0] === "."){
-      if(fileType.slice(-3) === "csv"){
-        this.importCSV()
-      }
-      else{
-        console.log("Unknown Dataformat")
-      }
-    }
-    else{
-      console.log("Wrong stripped")
-    }
-  }
+	/**  
+	 *  @description checks the ending of the file name of the import 
+	 */
+	checkFilename(){
+		const fileType = this.fileName.slice(-4)
+		if(fileType[0] === "."){
+			if(fileType.slice(-3) === "csv"){
+			this.importCSV()
+			}
+			else{
+			console.log("Unknown Dataformat")
+			}
+		}
+		else{
+			console.log("FormattingError: There was a mistake while checkinf the datatype")
+		}
+	}
 }
 
 exports.fileImport = fileImport;
