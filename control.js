5eb6656bc128cae747f23e2f26aa4b8607f734d9
EleckTroniiKz <73250162+EleckTroniiKz@users.noreply.github.com>
2022-02-23 10:18:48 +0000

    Added the menus (beside Application)

--- control.js
+++ control.js
@@ -1,5 +1,13 @@
 var term = require( 'terminal-kit' ).terminal ;
+const fs = require('fs');
+
+/*	Dieser Code-Snippet ist um die Dateien für den Import zu kriegen.
+var files = fs.readdirSync('./DataImport', {withFileTypes: true});
+console.log(files);
+process.exit()*/
 //TEST
+
+//Arrays mit den Menüs
 
 const mainMenu = [
                     "Exit",
@@ -8,14 +16,42 @@ const mainMenu = [
                     "Management Mode",
                     "Export Mode"]
 const managementModeMenu = [
-                       "Back to Main Menu",
-                       "Player settings",
-                       "Game settings"]
+                  	"Return to Main Menu",
+                    "Player management",
+                    "Game management",
+										"Gamenight planning"]
 
 const applicationModeMenu = [
-                    "Back to Main Menu",
-                    "Start Game"
+										"Return to Main Menu",
+										"Start Game"
 ]
+
+const managePlayersMenu = [
+                    "Return",
+                    "Add Player",
+                    "Edit Player",
+                    "Delete Player"
+]
+
+const editPlayersMenu = [ //NicetoHave
+										"Return",
+										"Edit Name",
+										"Edit Games"
+]
+
+const manageGamesMenu = [
+                    "Return",
+                    "Add Game",
+                    "Edit Game",
+                    "Delete Game"
+]
+
+const deleteGamesMenu = [
+                    "Return",
+                    "Delete Game from Player",
+                    "Delete Game Globally"
+]
+
 
 class Control {
 
@@ -31,6 +67,7 @@ postWelcome() {
 async postMainMenu() {
 
   let selectedIndex;
+	console.clear()
 
 	term(`>MainMenu<\n`) ;
 	term(`Please select a mode.\n`)
@@ -38,10 +75,8 @@ async postMainMenu() {
   const response = await term.singleColumnMenu( mainMenu ).promise;
   const selectedText = await response.selectedText;
   selectedIndex = await response.selectedIndex;
-  term( '\n' ).green("Selected:", selectedText); 
-  console.log();
-	console.log("THIS INDEX SHOULD GO OUT: " + selectedIndex);
-  
+  term( '\n' ).green("Selected: ", selectedText); 
+
 	return selectedIndex;
 }
   
@@ -49,7 +84,7 @@ async postManagementMode() {
 
   let selectedIndex;
 
-  await console.clear()
+  console.clear()
 
 	term(`>Management Mode<\n`) ;
 	term(`Please select a mode.\n`);
@@ -57,19 +92,15 @@ async postManagementMode() {
   const response = await term.singleColumnMenu( managementModeMenu ).promise;
   const selectedText = await response.selectedText;
   selectedIndex = await response.selectedIndex;
-  term( '\n' ).green("Selected:", selectedText);
-  console.log();
-	console.log("THIS INDEX SHOULD GO OUT: " + selectedIndex);
-  
+  term( '\n' ).green("Selected: ", selectedText);
 	return selectedIndex;
-
 }
 
 async postApplicationMode() {
 
   let selectedIndex;
 
-  await console.clear()
+  console.clear()
 
 	term(`>Application Mode<\n`) ;
 	term(`Please select a mode.\n`)
@@ -77,15 +108,83 @@ async postApplicationMode() {
   const response = await term.singleColumnMenu( applicationModeMenu ).promise;
   const selectedText = await response.selectedText;
   selectedIndex = await response.selectedIndex;
-  term( '\n' ).green("Selected:", selectedText);
-  console.log();
-	console.log("THIS INDEX SHOULD GO OUT: " + selectedIndex)
+  term( '\n' ).green("Selected: ", selectedText);
   
 	return selectedIndex;
 
 }
 
+async postManagePlayersMenu() {
+
+  let selectedIndex;
+  
+  console.clear()
+
+	term(`>Manage Players<\n`) ;
+	term(`Please select an Option.\n`);
+  
+  const response = await term.singleColumnMenu( managePlayersMenu ).promise;
+  const selectedText = await response.selectedText;
+  selectedIndex = await response.selectedIndex;
+  term( '\n' ).green("Selected: ", selectedText);
+  
+	return selectedIndex;
 }
 
+  //NicetoHave
+async postEditPlayersMenu() {
+
+  let selectedIndex;
+
+  console.clear()
+
+	term(`>Edit Player<\n`) ;
+	term(`Please select an Option.\n`);
+  
+  const response = await term.singleColumnMenu( editPlayersMenu ).promise;
+  const selectedText = await response.selectedText;
+  selectedIndex = await response.selectedIndex;
+  term( '\n' ).green("Selected: ", selectedText);
+  
+	return selectedIndex;
+}
+
+async postManageGamesMenu() {
+
+  let selectedIndex;
+
+  console.clear()
+
+	term(`>Manage Games<\n`) ;
+	term(`Please select an Option.\n`);
+  
+  const response = await term.singleColumnMenu( manageGamesMenu ).promise;
+  const selectedText = await response.selectedText;
+  selectedIndex = await response.selectedIndex;
+  term( '\n' ).green("Selected: ", selectedText);
+  
+	return selectedIndex;
+}
+
+async postDeleteGamesMenu() {
+
+  let selectedIndex;
+
+  console.clear()
+
+	term(`>Delete Games<\n`) ;
+	term(`Please select an Option.\n`);
+  
+  const response = await term.singleColumnMenu( deleteGamesMenu ).promise;
+  const selectedText = await response.selectedText;
+  selectedIndex = await response.selectedIndex;
+  term( '\n' ).green("Selected: ", selectedText);
+  
+	return selectedIndex;
+}
+
+  
+
+}
 
 exports.Control = Control
--- index.js
+++ index.js
@@ -1,58 +1,121 @@
 const {Boardgame, Player} = require("./applicationMode");
 const {DataHandler} = require('./dataHandler');
 const {Control} = require("./control");
+const {MODES, MANAGEMENT_MODES, MANAGEMENT_PLAYERS_MODES, EDIT_PLAYERS, MANAGEMENT_GAMES_MODES, DELETE_GAME} = require("./enums/enum.js")
+
 let control = new Control();
 
 //let session = new DataHandler('dos.csv')
 control.postWelcome();
+mainLoop();
 
-function showPage(x){
-	//
+async function gamesManagementLoop(mode_index){
+	
+	switch(mode_index){
+			case MANAGEMENT_GAMES_MODES.RETURN:
+				mainLoop(MODES.MANAGEMENT)
+				break;
+		case MANAGEMENT_GAMES_MODES.ADD:
+			//add GAME
+			//Input a Name for the game. Then choose if it should be added to a existing player or create a new player
+			break;
+		case MANAGEMENT_GAMES_MODES.EDIT:
+			//edit game
+			//first show list of games (ID + name). Then choose which one should be edited 
+			//After selection, choose a player with that game and change the name either for one person or globally
+			break;
+		case MANAGEMENT_GAMES_MODES.DELETE:
+			//delete Game
+			//Either for a player or globally
+			break;
+		default:
+			console.log("Something wrong with the index!")
+			break;
+	}
 }
-async function loop() {
-  let x = await control.postMainMenu();
-  console.log("INDEX: " + x);
-	switch(x) {
 
-		case 0:
+async function playerManagementLoop(mode_index){
+	console.log(mode_index === MANAGEMENT_PLAYERS_MODES.RETURN)
+	switch(mode_index){
+			case MANAGEMENT_PLAYERS_MODES.RETURN:
+				mainLoop(MODES.MANAGEMENT)
+				break;
+			case MANAGEMENT_PLAYERS_MODES.ADD:
+				//add Player
+				//Input for Player Name, and the Games from the Player
+				break;
+			case MANAGEMENT_PLAYERS_MODES.EDIT:
+				//edit Player
+				//Show list of players and give out the IDs + Names to select.
+				//After selection, choose what will be edited: Name, Games
+				break;
+			case MANAGEMENT_PLAYERS_MODES.DELETE:
+				//delete Player
+				//Show list of player and give out the IDs + Names to select.
+				//After selection, ask if the user is sure they want to delete that user
+				//YES --> delete |No --> RETURN
+				break;
+			default:
+				console.log("Something wrong with the index!55")
+				break;
+	}
+}
+
+async function managementLoop(mode_index){
+	let managementIndex;
+	switch(mode_index){
+		case MANAGEMENT_MODES.RETURN:
+			mainLoop();
+			break;
+		case MANAGEMENT_MODES.MANAGE_PLAYERS:
+      managementIndex = await control.postManagePlayersMenu();
+			playerManagementLoop(managementIndex);
+		  break;
+		case MANAGEMENT_MODES.MANAGE_GAMES:
+			managementIndex = await control.postManageGamesMenu();
+			gamesManagementLoop(managementIndex);
+			break;
+		case MANAGEMENT_MODES.PLAN_GAMENIGHT:
+			//Plan Gamenight
+			break;
+		default:
+			console.log("Something wrong with the index!")
+			break;
+	}
+}
+
+async function applicationLoop(mode_index){
+	//TO DO
+}
+
+async function mainLoop(mainIndex = null) {
+	if(!mainIndex){
+		mainIndex = await control.postMainMenu()
+	}
+  
+	switch(mainIndex) {
+		case MODES.EXIT:
 			console.log("EXIT");
       process.Exit();
 			break;
-
-		case 1: //Import Mode
+		case MODES.IMPORT: //Import Mode
 			break;
-
-		case 2: // Application Mode
-       let z =  await control.postApplicationMode();
-			    break;
-
-		case 3: //Management Mode
-        let y = await control.postManagementMode();
-		      break;
-
-  	case 4: //Export Mode
-		    	break;
-
+		case MODES.APPLICATION: // Application Mode
+      let applicationIndex = await control.postApplicationMode();	
+			applicationLoop(applicationIndex)
+	    break;
+		case MODES.MANAGEMENT: //Management Mode
+      let managementIndex = await control.postManagementMode();
+			console.log(managementIndex)
+			managementLoop(managementIndex);
+      break;
+  	case MODES.EXPORT: //Export Mode
+	  	break;
 		default:
-			console.log("Something wrong");
+			console.log("Something wrong with the index!");
 			break;
 	}
-	/**
-	* wenn x == 0 consle.log(exit), x==1 console.log(import mode), ...
-*/
-
-  // TODO: Rest des Programms
 }
-
-loop();
-
-//session.checkFilename()
-//session.deleteGame(2141, 'Magic')
-//session.deleteUser(757)
-//session.addGame(9598, 'Call of Duty')
-//console.log(session.getUserList())
-//session.addUser('Patrick', ['CSGO', 'Minecraft', 'Black Stories'])
-//console.log(session.getUserList())
 
 /*let game = new Boardgame("Wizard");
 console.log(game.getName());
